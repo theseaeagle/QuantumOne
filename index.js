8eeb7357896e8adb09ba546fd1ce0d0a2f167ebe
Mohamed Shaamil <shaamil@users.noreply.github.com>
2019-02-01 21:53:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.launch(function(request, response) {
          console.log("Quantum One Launched");
          response.say("Welcome to Quantum One! " + nickname +" Quantum One with it's PC client, can control your computer!");
       })();
-    }
+   
 });
 
 
