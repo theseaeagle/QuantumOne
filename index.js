0d2f0ffc894cb969176cb6b49308f0d68f8dafe3
Mohamed Shaamil <shaamil@users.noreply.github.com>
2019-02-01 19:03:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -51,13 +51,13 @@ db.serialize(function(){
 
 app.launch(function(request, response) {
   
-  launch(request);
+  launch(request,response);
   
   console.log("Quantum One Launched");
   //response.say("Welcome to Quantum One! Quantum One with it's PC client, can control your computer!");
 });
 
-function launch(request){
+function launch(request,reponse){
   var session = request.getSession()
   let accessToken = session.accessToken;
   let options = {
