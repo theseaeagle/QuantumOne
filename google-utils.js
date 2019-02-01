92158eb266b2c434744dc5462768af79dd5df777
Mohamed Shaamil <shaamil@users.noreply.github.com>
2019-02-01 12:06:00 +0500

    google-utils.js

---
+++ google-utils.js
@@ -0,0 +1,27 @@
+/**
+ * This scope tells google what information we want to request.
+ */
+const defaultScope = [
+  'https://www.googleapis.com/auth/plus.me',
+  'https://www.googleapis.com/auth/userinfo.email',
+];
+
+/**
+ * Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
+ */
+function getConnectionUrl(auth) {
+  return auth.generateAuthUrl({
+    access_type: 'offline',
+    prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
+    scope: defaultScope
+  });
+}
+
+/**
+ * Create the google url to be sent to the client.
+ */
+function urlGoogle() {
+  const auth = createConnection(); // this is from previous step
+  const url = getConnectionUrl(auth);
+  return url;
+}
